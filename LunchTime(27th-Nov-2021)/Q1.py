bc0cc9bf64c05228a0fcbc585f103ea829943a8d
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-11-27 19:47:02 +0530

    Create Q1.py

---
+++ LunchTime(27th-Nov-2021)/Q1.py
@@ -0,0 +1,8 @@
+#Problem Code:CSTOCK
+for i in range(int(input())):
+    s,a,b,c=map(int,input().split())
+    c=s+s*(c/100)
+    if c>=a and c<=b:
+        print("YES")
+    else:
+        print("NO")
