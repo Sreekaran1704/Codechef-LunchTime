6de0812c0a4aa85c504dc976d836798208306ddb
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-11-27 20:48:45 +0530

    Create Q2.py

---
+++ LunchTime(27th-Nov-2021)/Q2.py
@@ -0,0 +1,12 @@
+#Problem Code:MINPIZZAS
+def gcd(a,b):
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
+for i in range(int(input())):
+    n,k=map(int,input().split())
+    p=0
+    g=gcd(n,k)
+    p=n//g
+    print(p)
